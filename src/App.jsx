d61651a0907cfea4be7e19bfb4aeb5609cc14145
redouane <benbrahimredouane664@gmail.com>
2026-04-01 11:32:13 +0100

    feat: enhance App component with task management functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,52 @@
-
-
+import { useState } from "react";
+import Tasklist from "./components/TaslList";
+import TaskInput from "./components/TaskInput";
 
 function App() {
 
-  return (
-    <>
-      <h1>prodactivity dashboard</h1>
+  const [tasks, setTasks] = useState([]);
 
+
+  const addTask = (title) => {
+    const newTask = {
+      id : Date.now(),
+      title : title,
+      completed : false,
+      create_at : new Date().toLocaleDateString()
+    };
+
+     setTasks([...tasks,newTask]);
+
+  };
+
+  const toggleTask = (id) =>{
+    const updatedTasks = tasks.map((task)=>
+    task.id === id ? {...task, completed: !task.completed} : task
+    );
+    setTasks(updatedTasks);
+
+  };
+
+  const deleteTask = (id) =>{
+    const filtredTasks = tasks.filter((task)=> task.id !== id);
+    setTasks(filtredTasks);
+
+  };
+
+ 
+
+  return (
+
+    <>
+      <nav>
+        <h1>hermes</h1>
+      </nav>
+
+      <h1>Dashboard</h1>
+      <TaskInput addTask ={addTask}  />
+      <Tasklist tasks={tasks} />  
     </>
+
   );
 }
-
-export default App;
+export default App
