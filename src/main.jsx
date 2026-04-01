a61ee3c1628f9b76a06948768494a38d36e2bd24
redouane <benbrahimredouane664@gmail.com>
2026-04-01 11:32:38 +0100

    feat: update main component structure by removing Header and organizing imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Header from './assets/Header.jsx'
+import TaskInput from './components/TaskInput.jsx'
+import Tasklist from './components/TaslList.jsx'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Header/>
-    <App/>
+ 
+   
+   <StrictMode>
+    <App/> 
+  </StrictMode>
     
-  </StrictMode>,
+ 
 )
