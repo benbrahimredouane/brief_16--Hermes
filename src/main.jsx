208a664596f47bf16c1aec10e6877f7bbad95c04
redouane <benbrahimredouane664@gmail.com>
2026-04-02 11:24:32 +0100

    style: format code and fix import statement for Tasklist component

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import TaskInput from './components/TaskInput.jsx'
-import Tasklist from './components/TaslList.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import TaskInput from "./components/TaskInput.jsx";
+import Tasklist from "./components/TaskList.jsx";
 
-
-createRoot(document.getElementById('root')).render(
- 
-   
-   <StrictMode>
-    <App/> 
-  </StrictMode>
-    
- 
-)
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
