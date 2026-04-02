fbf4be751b8f51db7be3fed91fb68dee501c5843
redouane <benbrahimredouane664@gmail.com>
2026-04-02 11:28:59 +0100

    fix: correct import statement for Tasklist component and improve code formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,58 @@
 import { useState } from "react";
-import Tasklist from "./components/TaslList";
+import Tasklist from "./components/TaskList";
 import TaskInput from "./components/TaskInput";
+import Filter from "./components/Filter";
 
 function App() {
-
   const [tasks, setTasks] = useState([]);
-
+  const [filter, setFilter] = useState('all')
 
   const addTask = (title) => {
     const newTask = {
-      id : Date.now(),
-      title : title,
-      completed : false,
-      create_at : new Date().toLocaleDateString()
+      id: Date.now(),
+      title: title,
+      completed: false,
+      create_at: new Date().toLocaleDateString(),
     };
 
-     setTasks([...tasks,newTask]);
-
+    setTasks([...tasks, newTask]);
   };
 
-  const toggleTask = (id) =>{
-    const updatedTasks = tasks.map((task)=>
-    task.id === id ? {...task, completed: !task.completed} : task
+  const toggleTask = (id) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === id ? { ...task, completed: !task.completed } : task,
     );
     setTasks(updatedTasks);
-
   };
 
-  const deleteTask = (id) =>{
-    const filtredTasks = tasks.filter((task)=> task.id !== id);
+  const deleteTask = (id) => {
+    const filtredTasks = tasks.filter((task) => task.id !== id);
     setTasks(filtredTasks);
-
   };
 
- 
+    const filteredTasks = tasks.filter(task => {
+    if (filter === 'completed') return task.completed === true
+    if (filter === 'pending') return task.completed === false
+    return true 
+  })
 
   return (
-
     <>
       <nav>
         <h1>hermes</h1>
       </nav>
 
       <h1>Dashboard</h1>
-      <TaskInput addTask ={addTask}  />
-      <Tasklist tasks={tasks} />  
-    </>
+      <TaskInput addTask={addTask} />
 
+        <Filter 
+          filter={filter} 
+          setFilter={setFilter}
+        />
+
+
+      <Tasklist tasks={filteredTasks} toggleTask={toggleTask} deleteTask={deleteTask}  />
+    </>
   );
 }
-export default App
+export default App;
