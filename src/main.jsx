2a2c479693f57bd41b15f235c1ef6429b2b3d55c
redouane <benbrahimredouane664@gmail.com>
2026-04-05 19:25:10 +0100

    choore: add header from components

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import TaskInput from "./components/TaskInput.jsx";
-import Tasklist from "./components/TaskList.jsx";
+import Header from "./components/header.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Header/>
     <App />
   </StrictMode>,
 );
